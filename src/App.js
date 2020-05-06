6101dacc84cd2f83e6c514e6fab38b2785228763
Andy Martinez <martinez1751@gmail.com>
2020-05-06 12:01:20 -0400

    changed h1 to header in nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router } from '@reach/router';
+import { Router, Link } from '@reach/router';
 import SearchParams from './SearchParams';
 import Details from './Details';
 
@@ -8,16 +8,18 @@ const App = () => {
 
 return (
     <React.StrictMode>
-<div>
-<h1>Adopt Me</h1>
-<Router>
-<SearchParams path="/" />
-<Details path="/details/:id" />
-</Router>
- 
-
-</div>
-</React.StrictMode>
+        <div>
+            <header>
+                <Link to="/">
+                Adopt Me
+                </Link>
+            </header>
+            <Router>
+                <SearchParams path="/" />
+                <Details path="/details/:id" />
+            </Router>
+        </div>
+    </React.StrictMode>
 )
 };
 
--- src/Results.js
+++ src/Results.js
@@ -9,15 +9,15 @@ const Results = ({ pets }) => {
          ) : (
              pets.map(pet => {
                  return (
-                 <Pet
-                 animal={pet.type}
-                 key={pet.id}
-                 name={pet.name}
-                 breed={pet.breeds.primary}
-                 media={pet.photos}
-                location={`${pet.contact.address.city}, ${pet.contact.address.state}`}
-                id={pet.id} 
-                />
+                    <Pet
+                    animal={pet.type}
+                    key={pet.id}
+                    name={pet.name}
+                    breed={pet.breeds.primary}
+                    media={pet.photos}
+                    location={`${pet.contact.address.city}, ${pet.contact.address.state}`}
+                    id={pet.id} 
+                    />
              );
           })
          )}
